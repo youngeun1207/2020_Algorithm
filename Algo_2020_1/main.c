c018b063ac6d30338db00e2dcccad5a53a6cf6d7
youngeun1207 <yesheen98@gmail.com>
2020-04-26 23:07:57 +0900

    Initial Commit

---
+++ Algo_2020_1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Algo_2020_1
+//
+//  Created by 신영은 on 2020/04/26.
+//  Copyright © 2020 신영은. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
