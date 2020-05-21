8d28992b3745ab897fc768ee689e716833f3a095
youngeun1207 <yesheen98@gmail.com>
2020-05-22 00:55:01 +0900

    add comments

--- HW2/HW2/2-1.c
+++ HW2/HW2/2-1.c
@@ -1,4 +1,3 @@
-//
 //  2-1.c
 //  HW2
 //  Rod Cutting
@@ -19,16 +18,18 @@ int main(int argc, const char * argv[]) {
         printf("error: out of range\n");
         return 0;
     }
+    // 길이 당 Cost 저장
     int* costs = CreateArray(length);
-    
     for(int i = 1; i <= length; i++){
         int cost;
         fscanf(fi, "%d", &cost);
         costs[i] = cost;
     }
+    // 각 길이 별 최대 Cost 저장
     int* MaxCost = CreateArray(length);
+    // 각 길이 별 최대 Cost가 나오는 길이 저장
     int* MaxLen = CreateArray(length);
-    
+    // 상향식 방법
     BottomUpCutRod(costs, length, MaxCost, MaxLen);
     
     // Print Cut Rod Solution
@@ -44,8 +45,10 @@ int main(int argc, const char * argv[]) {
 
 void BottomUpCutRod(int* costs, int length, int* MaxCost, int* MaxLen){
     int max;
+    // 길이 i의 MaxCost 구하기
     for(int i = 1; i <= length; i++){
-        max = INT_MIN;
+        max = INT_MIN; // 계산되지 않은 값 : -∞
+        // i를 j와 i-j로 나눴을 때 최대 cost가 나오는 j 구하기
         for(int j = 1; j <= i; j++){
             if(max < costs[j] + MaxCost[i-j]){
                 max = costs[j] + MaxCost[i-j];
@@ -58,6 +61,6 @@ void BottomUpCutRod(int* costs, int length, int* MaxCost, int* MaxLen){
 
 int* CreateArray(int length){
     int* arr = (int*)malloc(sizeof(int)*(length+1));
-    arr[0] = 0;
+    arr[0] = 0; // 길이가 0인 막대는 cost 0
     return arr;
 }
--- HW2/HW2/2-2.c
+++ HW2/HW2/2-2.c
@@ -1,4 +1,3 @@
-//
 //  2-2.c
 //  HW2
 //  Longest Common Subsequence
@@ -15,27 +14,29 @@
 #define NONE -1
 
 typedef struct _Element{
-    int Direction;
-    char Element;
+    int Direction;  // 화살표
+    int Length;     // 길이
 }Element;
 
 Element** Create_Table(int x, int y);
-void LCS_Len(char* x, char* y, Element** table);
+void LCS_Table(char* x, char* y, Element** table);
 void Print_LCS(Element** table, char* x, int i, int j);
 
 int main(int argc, const char * argv[]) {
     FILE *fi = fopen("./input2-2.txt", "r");
+    // 개행문자를 고려하여 최대값 +1로 동적할당
     char* x = malloc(MAX+1);
     char* y = malloc(MAX+1);
     fgets(x, MAX, fi);
     fgets(y, MAX, fi);
-    
+    fclose(fi);
+    // 이차원배열 선언 및 초기화
     Element** table = Create_Table((int)strlen(x), (int)strlen(y));
-    LCS_Len(x, y, table);
+    // LCS 추적 이차원배열 만들기
+    LCS_Table(x, y, table);
+    // 역추적하여 LCS 출력
     Print_LCS(table, x, (int)strlen(x)-1, (int)strlen(y)-1);
     printf("\n");
-    fclose(fi);
-    
     return 0;
 }
 
@@ -43,41 +44,47 @@ Element** Create_Table(int x, int y){
     Element** table = malloc(sizeof(struct _Element) * x);
     for(int i = 0; i < x; i++){
         table[i] = malloc(sizeof(struct _Element) * y);
-        table[i][0].Element = 0;
+        // init table
+        table[i][0].Length = 0;
         table[i][0].Direction = NONE;
     }
+    // init table
     for(int i = 0; i < y; i++){
-        table[0][i].Element = 0;
+        table[0][i].Length = 0;
         table[0][i].Direction = NONE;
     }
     return table;
 }
-void LCS_Len(char* x, char* y, Element** table){
+void LCS_Table(char* x, char* y, Element** table){
     int x_len = (int)strlen(x) - 1;
     int y_len = (int)strlen(y) - 1;
     
     for(int i = 0; i < x_len; i++){
         for(int j = 0; j < y_len; j++){
+            // LCS에 해당하는 원소
             if(x[i] == y[j]){
-                table[i+1][j+1].Element = table[i][j].Element + 1;
+                table[i+1][j+1].Length = table[i][j].Length + 1; // LCS 길이 증가
                 table[i+1][j+1].Direction = DIAGONAL;
             }
-            else if(table[i][j+1].Element >= table[i+1][j].Element){
-                table[i+1][j+1].Element = table[i][j+1].Element;
+            // 위가 왼쪽보다 크거나 같은 경우
+            // 위와 왼쪽의 값이 같은 경우 위의 값을 선택해야하므로 UP 먼저 설정
+            else if(table[i][j+1].Length >= table[i+1][j].Length){
+                table[i+1][j+1].Length = table[i][j+1].Length;
                 table[i+1][j+1].Direction = UP;
             }
+            // 왼쪽이 더 큰 경우
             else{
-                table[i+1][j+1].Element = table[i+1][j].Element;
+                table[i+1][j+1].Length = table[i+1][j].Length;
                 table[i+1][j+1].Direction = LEFT;
             }
         }
     }
 }
-
-void Print_LCS(Element** table, char* x, int i, int j){
+void Print_LCS(Element** table, char* x, int i, int j){ // 화살표를 따라가며 재귀호출
     if(i == 0 || j == 0){
         return;
     }
+    // LCS에 해당하는 원소
     if(table[i][j].Direction == DIAGONAL){
         Print_LCS(table, x, i-1, j-1);
         printf("%c",x[i-1]);
@@ -85,7 +92,7 @@ void Print_LCS(Element** table, char* x, int i, int j){
     else if(table[i][j].Direction == UP){
         Print_LCS(table, x, i-1, j);
     }
-    else if(table[i][j].Direction == LEFT){
+    else{ // table[i][j].Direction == LEFT
         Print_LCS(table, x, i, j-1);
     }
 }
